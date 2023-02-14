c28d9d28229e043c121852e0c548dabc37a5a376
Juan Carlos Villasenor Derbez <juancarlos@ucsb.edu>
2023-02-14 09:04:03 -0800

    Added set up script

---
+++ scripts/00_set_up.R
@@ -0,0 +1,23 @@
+################################################################################
+# title
+################################################################################
+#
+# Juan Carlos Villaseñor-Derbez
+# juancvd@stanford.edu
+# date
+#
+# Description
+#
+################################################################################
+
+## SET UP ######################################################################
+
+# Install remotes
+install.packages("remotes")
+# Install startR
+remotes::install_github("emlab-ucsb/startR")
+# Build directories
+startR::create_local_dirs(other_dirs = c("data", "data/raw", "data/processed", "data/output"))
+
+
+## PROCESSING ##################################################################

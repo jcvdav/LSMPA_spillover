a5f32c944fabd243a9dd2ee887e9aa5c0f6cd6c6
Juan Carlos Villasenor Derbez <juancarlos@ucsb.edu>
2023-08-03 16:28:20 -0700

    Added updated quarterly and annual pannels

---
+++ scripts/02_make_data/03_make_annual_all_rfmos.R
@@ -0,0 +1,51 @@
+################################################################################
+# title
+################################################################################
+#
+# Juan Carlos Villaseñor-Derbez
+# juancvd@stanford.edu
+# date
+#
+# Description
+#
+################################################################################
+
+## SET UP ######################################################################
+
+# Load packages ----------------------------------------------------------------
+
+# Load data --------------------------------------------------------------------
+quarterly_all_rfmos <- readRDS(file = here("data", "processed", "quarterly_all_rfmos.rds"))
+
+## PROCESSING ##################################################################
+
+# X ----------------------------------------------------------------------------
+annual_all_rfmos <- quarterly_all_rfmos %>%
+  select(-contains("cpue_")) %>%
+  group_by(rfmo, year, gear, flag, lat, lon, effort_measure, effort) %>%
+  summarize(alb_mt = sum(alb_mt, na.rm = T),
+            bet_mt = sum(bet_mt, na.rm = T),
+            bft_mt = sum(bft_mt, na.rm = T),
+            lot_mt = sum(lot_mt, na.rm = T),
+            pbf_mt = sum(pbf_mt, na.rm = T),
+            sbf_mt = sum(sbf_mt, na.rm = T),
+            skj_mt = sum(skj_mt, na.rm = T),
+            tot_mt = sum(tot_mt, na.rm = T),
+            yft_mt = sum(yft_mt, na.rm = T)) %>%
+  ungroup() %>%
+  mutate(cpue_alb = alb_mt / effort,
+         cpue_bet = bet_mt / effort,
+         cpue_bft = bft_mt / effort,
+         cpue_lot = lot_mt / effort,
+         cpue_pbf = pbf_mt / effort,
+         cpue_sbf = sbf_mt / effort,
+         cpue_skj = skj_mt / effort,
+         cpue_tot = tot_mt / effort,
+         cpue_yft = yft_mt / effort)
+
+
+## EXPORT ######################################################################
+
+# X ----------------------------------------------------------------------------
+saveRDS(object = annual_all_rfmos,
+        file = here("data", "processed", "annual_all_rfmos.rds"))
---
+++ scripts/02_make_data/05_build_annual_panels.R
@@ -0,0 +1,59 @@
+################################################################################
+# title
+################################################################################
+#
+# Juan Carlos Villaseñor-Derbez
+# juancvd@stanford.edu
+# date
+#
+# Description
+#
+################################################################################
+
+## SET UP ######################################################################
+
+# Load packages ----------------------------------------------------------------
+pacman::p_load(
+  here,
+  tidyverse
+)
+
+# Load data --------------------------------------------------------------------
+distance_grid <- read_rds(file = here("data", "processed", "distance_grid.rds"))
+rfmo_data <- readRDS(here("data", "processed", "annual_all_rfmos.rds"))
+
+## PROCESSING ##################################################################
+
+# X ----------------------------------------------------------------------------
+combined <- rfmo_data %>%
+  left_join(distance_grid, by = c("lon", "lat")) %>%
+  mutate(id = paste(lat, lon, sep = "_"),
+         event = year - year_enforced,
+         post = 1 * (event >= 0))
+
+# I define "main players" as those with the most EFFORT since MPA implemented
+main_players <- combined %>%
+  filter(year > year_enforced) %>%
+  filter(cpue_tot > 0) %>%
+  drop_na(flag) %>%
+  filter(gear %in% c("purse_seine", "longline")) %>%
+  group_by(rfmo, name, gear, flag) %>%
+  summarize(effort = sum(effort)) %>%
+  ungroup() %>%
+  group_by(rfmo, name, gear) %>%
+  filter(effort == max(effort)) %>%
+  ungroup() %>%
+  select(rfmo, name, gear, flag)
+
+# Keeping only the "main players", retains about 11% of the data (77488 / 653522)
+restricted <- combined %>%
+  inner_join(main_players, by = c("rfmo", "name", "gear", "flag")) %>%
+  mutate(bin = factor(ceiling(dist / 100)))
+
+## EXPORT ######################################################################
+
+# X ----------------------------------------------------------------------------
+saveRDS(object = restricted,
+        file = here("data", "processed", "restricted_annual_panel.rds"))
+saveRDS(object = combined,
+        file = here("data", "processed", "annual_panel.rds"))

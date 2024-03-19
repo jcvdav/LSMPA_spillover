2a72c819e6472b63a3eededf741a834ab37e1d32
Juan Carlos Villasenor Derbez <juancarlos@ucsb.edu>
2024-03-19 13:39:42 -0500

    Updated table B1 with PS info only

--- scripts/05_robustness_checks/01_main_regs_diffrence_in_means.R
+++ scripts/05_robustness_checks/01_main_regs_diffrence_in_means.R
@@ -41,6 +41,7 @@ relevant_mpa_gear_reg <- readRDS(file = here("data", "output", "relevant_mpa_gea
 main_reg_wo_fe <- feols(log(cpue_tot) ~ post + near + post:near,
                         panel.id = ~id + year,
                         vcov = "iid",
+                        subset = ~gear == "purse_seine",
                         split = ~nice_gear,
                         data = annual_panel)
 
@@ -49,6 +50,7 @@ relevant_mpa_gear_reg_wo_fe <- feols(log(cpue_tot) ~ post + near + post:near,
                                      panel.id = ~id + year,
                                      vcov = "iid",
                                      split = ~nice_gear,
+                                     subset = ~gear == "purse_seine",
                                      data = most_relevant_panel)
 
 ## VISUALIZE ###################################################################
@@ -63,12 +65,12 @@ gm <- tribble(~raw, ~clean, ~fmt,
               "FE: wdpaid^gear^year", "FE: MPA-Gear-Year", 0
 )
 
-panelsummary(list(main_reg_wo_fe[[1]], main_reg[[4]][[2]],
-                  main_reg_wo_fe[[2]], main_reg[[4]][[3]]),
-             list(relevant_mpa_gear_reg_wo_fe[[1]], relevant_mpa_gear_reg[[4]][[2]],
-                  relevant_mpa_gear_reg_wo_fe[[2]], relevant_mpa_gear_reg[[4]][[3]]),
-             panel_labels = c("Panel A: All data (23 LSMPA-gear combinations; 14 LSMPAs)",
-                              "Panel B: Subsample (14 LSMPA-gear combinations; 11 LSMPAs)"),
+# Extract relevant models from each object. Recall that the only model with
+# a full set of fixed-effects and no NINO covariates is in the fourth position
+panelsummary(list(main_reg_wo_fe[[1]], main_reg[[4]]),
+             list(relevant_mpa_gear_reg_wo_fe[[1]], relevant_mpa_gear_reg[[4]]),
+             panel_labels = c("Panel A: All data (9 LSMPAs)",
+                              "Panel B: Subsample (6 LSMPAs)"),
              stars = "econ",
              pretty_num = T,
              collapse_fe = T,
@@ -80,12 +82,10 @@ panelsummary(list(main_reg_wo_fe[[1]], main_reg[[4]][[2]],
              without fixed-effects when estimating the spillover effects of
              Large-Scale Marine Protected Areas on catch-per-unit effort of tuna
              fisheries.} Coefficients are difference-in-difference estimates for
-             the change in CPUE. Columns 1 and 2 present models fit to purse
-             seine data only, and columns 3 and 4 present models fit to longline
-             data only. Models with fixed-effects (columns 2 and 4) are the same as the ones
-             presented in the main text. Note that R$^2$ values are always higher for models with fixed-effects.") %>%
-  add_header_above(c(" " = 1,
-                     "Purse seine" = 2,
-                     "Longline" = 2)) %>%
+             the change in CPUE. Panel A shows results for all data, and panel B
+             shows results for relevant MPAs only. The first column shows coefficient
+             estimates with models that do not use fixed-effects. The second column
+             shows the same values as the main text, which uses fixed-effects.
+             Note that R$^2$ values are always higher for models with fixed-effects.") %>%
   footnote("$* p < 0.1, ** p < 0.05, *** p < 0.01$", escape = F) %>%
   cat(file = here("results", "tab", "tabS1_main_regression_table.tex"))

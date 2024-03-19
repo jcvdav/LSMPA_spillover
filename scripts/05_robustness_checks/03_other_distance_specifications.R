a627af114c87d9ab03b874acf487bd8270174020
Juan Carlos Villasenor Derbez <juancarlos@ucsb.edu>
2024-03-19 15:52:37 -0500

    Updated other distance specifications

--- scripts/05_robustness_checks/03_other_distance_specifications.R
+++ scripts/05_robustness_checks/03_other_distance_specifications.R
@@ -25,29 +25,27 @@ pacman::p_load(
 source(here("scripts/00_set_up.R"))
 
 # Load data --------------------------------------------------------------------
-most_relevant_panel_multiple_distances <- readRDS(here("data", "processed", "annual_relevant_mpa_gears_and_distances_sensitivity_estimation_panel.rds"))
+most_relevant_panel_multiple_distances <- readRDS(here("data", "processed", "annual_relevant_mpa_gears_and_distances_sensitivity_estimation_panel.rds")) %>%
+  filter(gear == "purse_seine")
 
 ## PROCESSING ##################################################################
 
 # X ----------------------------------------------------------------------------
-near_100 <- feols(log(cpue_tot) ~ post + near_100 +  i(post, near_100, 0) | id + flag ^ gear + wdpaid ^ gear ^ year,
+near_100 <- feols(log(cpue_tot) ~ post + near_100 +  post * near_100 | id + flag ^ gear + wdpaid ^ gear ^ year,
                   panel.id = ~id + year,
                   vcov = conley(cutoff = 200),
-                  fsplit = ~nice_gear,
                   subset = ~!is.na(near_100),
                   data = most_relevant_panel_multiple_distances)
 
-near_200 <- feols(log(cpue_tot) ~ post + near_200 + i(post, near_200, 0) | id + flag ^ gear + wdpaid ^ gear ^ year,
+near_200 <- feols(log(cpue_tot) ~ post + near_200 + post * near_200 | id + flag ^ gear + wdpaid ^ gear ^ year,
                   panel.id = ~id + year,
                   vcov = conley(cutoff = 200),
-                  fsplit = ~nice_gear,
                   subset = ~!is.na(near_200),
                   data = most_relevant_panel_multiple_distances)
 
-near_300 <- feols(log(cpue_tot) ~ post + near_300 + i(post, near_300, 0) | id + flag ^ gear + wdpaid ^ gear ^ year,
+near_300 <- feols(log(cpue_tot) ~ post + near_300 + post*near_300 | id + flag ^ gear + wdpaid ^ gear ^ year,
                   panel.id = ~id + year,
                   vcov = conley(cutoff = 200),
-                  fsplit = ~nice_gear,
                   subset = ~!is.na(near_300),
                   data = most_relevant_panel_multiple_distances)
 
@@ -69,16 +67,15 @@ panelsummary(near_100,
              caption = "\\label{tab:other_near_far}\\textbf{Testing for spillover effects using other definitions of near and far from an LSMPA boundary.}
              The table shows difference-in-difference estimates using alternative definitions of the near-far areas. In Panel A near is defined as 0-100 and
              far as 100-200 NM. In Panel B near is defined as 0-200 and far as 200-400 NM. Finally, in panel B near is defined as 0-300 and far as 300-600 NM.",
-             colnames = c("", "Combined", "Purse Seine", "Longline"),
              panel_labels = c("Panel A: 0:100 - 100:200 nautical miles",
                               "Panel B: 0:200 - 200:400 nautical miles",
                               "Panel C: 0:300 - 300:600 nautical miles"),
              stars = "econ",
              collapse_fe = T,
              format = "latex",
-             coef_map = c("post::1:near_100" = "Post x Distance",
-                          "post::1:near_200" = "Post x Distance",
-                          "post::1:near_300" = "Post x Distance"),
+             coef_map = c("post:near_100" = "Post x Distance",
+                          "post:near_200" = "Post x Distance",
+                          "post:near_300" = "Post x Distance"),
              pretty_num = T,
              gof_map = gm,
              hline_after = T) %>%
